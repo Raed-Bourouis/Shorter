2554eebde8a4979d2e1e0699a8ff6d5091055e4e
Raed-Bourouis <144831482+Raed-Bourouis@users.noreply.github.com>
2025-10-05 19:08:20 +0100

    init shortener

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Go URL shortener"})
+	})
+	err := r.Run(":9808")
+	if err != nil {
+		panic(fmt.Sprintf("Failed to start the web server - Error: %v", err))
+	}
+}
