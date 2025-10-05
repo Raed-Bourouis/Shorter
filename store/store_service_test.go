b011b9cf99cfa1cd4dcadcdd9a3b19f432a70cfe
Raed-Bourouis <144831482+Raed-Bourouis@users.noreply.github.com>
2025-10-05 20:31:06 +0100

    Store Service + tests

--- store/store_service_test.go
+++ store/store_service_test.go
@@ -17,7 +17,7 @@ func TestStoreInit(t *testing.T) {
 }
 
 func TestInsertionAndRetrieval(t *testing.T) {
-	initialLink := "https://www.guru3d.com/news-story/spotted-ryzen-threadripper-pro-3995wx-processor-with-8-channel-ddr4,2.html"
+	initialLink := "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs"
 	userUUId := "e0dba740-fc4b-4977-872c-d360239e6b1a"
 	shortURL := "Jsz4k57oAX"
 
